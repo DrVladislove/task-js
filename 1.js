8a2f31af6d62170d8ad04625d64e08a9395637f7
DrVladislove <52196631+DrVladislove@users.noreply.github.com>
2020-07-08 14:13:48 +0300

    task js

---
+++ 1.js
@@ -0,0 +1,46 @@
+var x = 237;
+var y = 10;
+
+/*
+Write next functions
+
+call them with arguments a and b
+and print the results via console.log
+
+1. Multiply(a, b)
+
+returns result of multiplying 2 numbers'
+
+2. Divide(a, b)
+
+returns result of division of first argument by second argument
+
+
+3. Sum(a, b)
+
+returns sum of 2 arguments
+
+
+*/
+
+//1
+function multiplying() {
+  var result = x * y;
+  return result;
+}
+
+//2
+function dividing() {
+  var result = x / y;
+  return result;
+}
+
+//3
+function sum() {
+  var result = x + y;
+  return result;
+}
+
+console.log(multiplying());
+console.log(dividing());
+console.log(sum());
