9ae1a0b384fbf1ff1f27c77c87bf1e4a7c4a1da3
DrVladislove <52196631+DrVladislove@users.noreply.github.com>
2020-07-16 16:28:04 +0300

    task 5 definition-or-symbol

---
+++ conditions/definition-or-symbol.js
@@ -0,0 +1,18 @@
+/*function definiton() {
+  charCodeAt(a);
+  if (charCodeAt(a) / 1) {
+    console.log("Число" + charCodeAt(a));
+  }
+}
+
+definiton();
+charCodeAt(66);
+*/
+
+function definition(a) {
+  var testChar = String.fromCharCode(a);
+  if ((a = String)) {
+    console.log(testChar);
+  }
+}
+definition(66);
