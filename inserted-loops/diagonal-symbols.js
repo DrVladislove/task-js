b275746523c01974d41cab7192cad25004249d4b
DrVladislove <52196631+DrVladislove@users.noreply.github.com>
2020-07-21 17:01:26 +0300

    diagonal symbols task

---
+++ inserted-loops/diagonal-symbols.js
@@ -0,0 +1,15 @@
+function diagonalSymbols() {
+  var i, j;
+  for (i = 0; i < 20; i++) {
+    let line = "";
+    for (j = 0; j < 20; j++) {
+      if (i == j || i == 19 - j) {
+        line += "x";
+      } else {
+        line += " ";
+      }
+    }
+    console.log(line);
+  }
+}
+diagonalSymbols();
