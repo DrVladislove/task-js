70554391c106583012a67597897813f7e397610a
DrVladislove <52196631+DrVladislove@users.noreply.github.com>
2020-07-27 17:32:39 +0300

    test-loops-1 correctins, conditional of task was added

--- test/operations-loops-1.js
+++ test/operations-loops-1.js
@@ -1,4 +1,8 @@
-var assert = require("assert");
+/*1. Генератор четных - нечетных чисел
+Создайте цикл, который будет случайным образом генерировать 5 чисел от 0 до 20.
+ Для каждого случая будет проверятся текущее число (четное или нечетное), и выводится результат на экран (например, 2 - четное).
+ Решение оформите в виде функции numberGenerator()
+ */
 function randomizer(min, max) {
   return Math.floor(min + Math.random() * (max + 1 - min));
 }
@@ -7,7 +11,7 @@ function numberGenerator() {
     var a = randomizer(1, 20);
     if (a % 2 == 0) {
       return a, "Четное";
-    } else if (a % 2) {
+    } else {
       return a, "Нечетное";
     }
   }
