d841445871374f4da7dd439ebdea72b4d9180c56
DrVladislove <52196631+DrVladislove@users.noreply.github.com>
2020-07-27 17:15:29 +0300

    test-conditions-2.js correction

--- test/test-conditions-2.js
+++ test/test-conditions-2.js
@@ -1,5 +1,4 @@
 const operations = require("./operations-conditions-2.js");
-const assert = require("assert");
 
 it("Выдает правильный язык conditions-task-2", () => {
   assert.equal(operations.helloWorld("en"), "Hello");
