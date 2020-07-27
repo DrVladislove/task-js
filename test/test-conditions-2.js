d632fd80e0855b8562ac180c42d7d8faaa829b3a
DrVladislove <52196631+DrVladislove@users.noreply.github.com>
2020-07-27 17:21:44 +0300

    condition of task added

--- test/test-conditions-2.js
+++ test/test-conditions-2.js
@@ -2,7 +2,7 @@
 Напишите функцию helloWorld которая: Принимает 1 аргумент, код языка (например "en", "es") верните "Hello, World" для данного языка(хотя бы три языка).
  По умолчанию возвращает русский вариант.
  */
-
+const assert = require("assert");
 const operations = require("./operations-conditions-2.js");
 
 it("Выдает правильный язык conditions-task-2", () => {
