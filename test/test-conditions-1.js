16c6c25e63fd323cf7f2c682394f60d1115fbabb
DrVladislove <52196631+DrVladislove@users.noreply.github.com>
2020-07-27 16:47:09 +0300

    test-conditions-1.js correction

--- test/test-conditions-1.js
+++ test/test-conditions-1.js
@@ -4,3 +4,15 @@ const assert = require("assert");
 it("Правильно определяет условие conditions-task-1", () => {
   assert.equal(operations.greaterNum(5, 10), 10);
 });
+
+function greaterNum(num1, num2) {
+  if (num1 > num2) {
+    return num1;
+  } else if (num1 < num2) {
+    return num2;
+  }
+}
+
+module.exports = {
+  greaterNum,
+};
