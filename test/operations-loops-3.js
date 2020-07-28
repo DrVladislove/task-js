b8a3976174e19be1c16f41f02c469a15d782350a
DrVladislove <52196631+DrVladislove@users.noreply.github.com>
2020-07-28 16:36:22 +0300

    operations-loops-3 corrections

--- test/operations-loops-3.js
+++ test/operations-loops-3.js
@@ -1,18 +1,30 @@
-var assert = require("assert");
+/* 3. Оценка результатов
+Cоздайте функцию checkAssignGrade:
+Проверьте результаты функции assignGrade из условных упражнений для каждого значения от 80 до 100.
+То есть создайте цикл for от 80 до 100 и вызывайте в нем вашу функцию, например assignGrade(81)
+*/
+
 function assignGrade(mark) {
-  if (mark == 80) {
-    return "Для i = 80 оценка 4";
+  if (mark >= 1 && mark <= 20) {
+    return "mark is 1";
+  } else if (mark >= 21 && mark <= 40) {
+    return "mark is 2";
+  } else if (mark >= 41 && mark <= 60) {
+    return "mark is 3";
+  } else if (mark >= 61 && mark <= 80) {
+    return "mark is 4";
   } else if (mark >= 81 && mark <= 100) {
-    return "Для i=" + mark + " " + "оценка 5";
+    return "mark is 5";
   }
 }
 
 function checkAssignGrade() {
   for (i = 85; i <= 100; i++) {
+    console.log(assignGrade(81));
     return i;
   }
 }
-
+checkAssignGrade();
 module.exports = {
   checkAssignGrade,
 };
