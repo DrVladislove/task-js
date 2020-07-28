083209f623e797bd2240d55eb24139696118d8ef
DrVladislove <52196631+DrVladislove@users.noreply.github.com>
2020-07-28 15:30:26 +0300

    corrections conditions-task-1

--- test/test-conditions-1.js
+++ test/test-conditions-1.js
@@ -1,18 +1,12 @@
+/*1. Какое из чисел больше?
+Напишите функцию greaterNum которая: принимает 2 аргумента(числа).
+Bозвращает наибольшее число из полученных в аргументы.
+ Вызовите два раза с разными парами чисел, и выведите в консоль (например, "Наибольшее число между 5 и 10: 10.").
+*/
+
 const operations = require("./operations-conditions-1.js");
 const assert = require("assert");
 
 it("Правильно определяет условие conditions-task-1", () => {
   assert.equal(operations.greaterNum(5, 10), 10);
 });
-
-function greaterNum(num1, num2) {
-  if (num1 > num2) {
-    return num1;
-  } else if (num1 < num2) {
-    return num2;
-  }
-}
-
-module.exports = {
-  greaterNum,
-};
