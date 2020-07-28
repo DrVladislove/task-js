6f8c1e1acc1bbe8eeb6f08e67728da16bb29b9fa
DrVladislove <52196631+DrVladislove@users.noreply.github.com>
2020-07-28 16:43:32 +0300

    test conditions 1 corrections

--- test/test-conditions-1.js
+++ test/test-conditions-1.js
@@ -8,5 +8,8 @@ const operations = require("./operations-conditions-1.js");
 const assert = require("assert");
 
 it("Правильно определяет условие conditions-task-1", () => {
-  assert.equal(operations.greaterNum(5, 10), 10);
+  assert.equal(operations.greaterNum(0, 1), 1);
+});
+it("Правильно определяет условие conditions-task-1", () => {
+  assert.equal(operations.greaterNum(1, 1), 1);
 });
