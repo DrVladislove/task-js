106a44f1a9ccb623773ef7fe7e4ecf4b5f62947a
DrVladislove <52196631+DrVladislove@users.noreply.github.com>
2020-07-28 17:21:44 +0300

    test conditions 3 corrections  (added more cases)

--- test/test-conditions-3.js
+++ test/test-conditions-3.js
@@ -4,5 +4,17 @@
 const operations = require("./operations-conditions-3.js");
 const assert = require("assert");
 it("Правильно определяет оценки conditions-task-3", () => {
-  assert.equal(operations.assignGrade(62, 75), "mark is 4");
+  assert.equal(operations.assignGrade(62), "mark is 4");
+});
+it("Правильно определяет оценки conditions-task-3", () => {
+  assert.equal(operations.assignGrade(81), "mark is 5");
+});
+it("Правильно определяет оценки conditions-task-3", () => {
+  assert.equal(operations.assignGrade(57), "mark is 3");
+});
+it("Правильно определяет оценки conditions-task-3", () => {
+  assert.equal(operations.assignGrade(35), "mark is 2");
+});
+it("Правильно определяет оценки conditions-task-3", () => {
+  assert.equal(operations.assignGrade(15), "mark is 1");
 });
