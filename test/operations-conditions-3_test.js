f86712cef1155cca1a75740c98b0746422b4dc90
DrVladislove <52196631+DrVladislove@users.noreply.github.com>
2020-07-29 17:01:51 +0300

    Change aswer for right tests (Task assignGrade)

--- test/operations-conditions-3_test.js
+++ test/operations-conditions-3_test.js
@@ -3,18 +3,21 @@
 */
 const operations = require("./operations-conditions-3.js");
 const assert = require("assert");
-it("Правильно определяет оценки conditions-task-3", () => {
-  assert.equal(operations.assignGrade(62), "mark is 4");
-});
-it("Правильно определяет оценки conditions-task-3", () => {
-  assert.equal(operations.assignGrade(81), "mark is 5");
-});
-it("Правильно определяет оценки conditions-task-3", () => {
-  assert.equal(operations.assignGrade(57), "mark is 3");
-});
-it("Правильно определяет оценки conditions-task-3", () => {
-  assert.equal(operations.assignGrade(35), "mark is 2");
-});
-it("Правильно определяет оценки conditions-task-3", () => {
-  assert.equal(operations.assignGrade(15), "mark is 1");
+
+describe("Постановщик оценок", function () {
+  it("Правильно определяет оценку (4)", () => {
+    assert.equal(operations.assignGrade(62), "mark is 4");
+  });
+  it("Правильно определяет оценку (5)", () => {
+    assert.equal(operations.assignGrade(81), "mark is 5");
+  });
+  it("Правильно определяет оценку (3)", () => {
+    assert.equal(operations.assignGrade(57), "mark is 3");
+  });
+  it("Правильно определяет оценку (2)", () => {
+    assert.equal(operations.assignGrade(35), "mark is 2");
+  });
+  it("Правильно определяет оценку (1)", () => {
+    assert.equal(operations.assignGrade(15), "mark is 1");
+  });
 });
