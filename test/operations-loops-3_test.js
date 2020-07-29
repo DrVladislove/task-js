6bc6af2d02ad30f8abf23d567fa1fdd9a088ce0c
DrVladislove <52196631+DrVladislove@users.noreply.github.com>
2020-07-29 14:39:14 +0300

    Renamed file for understanding

---
+++ test/operations-loops-3_test.js
@@ -0,0 +1,11 @@
+/* 3. Оценка результатов
+Cоздайте функцию checkAssignGrade:
+Проверьте результаты функции assignGrade из условных упражнений для каждого значения от 80 до 100.
+То есть создайте цикл for от 80 до 100 и вызывайте в нем вашу функцию, например assignGrade(81)
+*/
+
+const operations = require("./operations-loops-3");
+const assert = require("assert");
+it("Оценка результатов выполнена правильно loops-task-3", () => {
+  assert.equal(operations.checkAssignGrade(), i);
+});
