a776d41cacab88c2dcad3d81143cd19998ea0c76
DrVladislove <52196631+DrVladislove@users.noreply.github.com>
2020-07-29 14:37:37 +0300

    operations-loops-2 corrections (was added condition of task and repaired output of results)

---
+++ test/operations-loops-2_test.js
@@ -0,0 +1,38 @@
+/*2. Таблица умножения
+Создайте цикл, который будет выполнятся от 1 до 10. Используйте вложенный цикл for, что бы показать таблицу для каждого множителя от 1 до 10 (всего 100 результатов). 
+Решение оформите в виде функции multiplicationTable
+*/
+
+const operations = require("./operations-loops-2.js");
+var sinon = require("sinon");
+var expect = require("chai").expect;
+
+describe("multiplicationTable", function () {
+  var spy;
+
+  before(function () {
+    // runs before all tests in this block
+    spy = sinon.spy(console, "log");
+    operations.multiplicationTable();
+  });
+
+  after(function () {
+    // runs after all tests in this block
+    console.log.restore();
+  });
+
+  it("Выводит таблицу", function () {
+    var resultString;
+    for (var i = 1; i <= 10; i++) {
+      for (var j = 1; j <= 10; j++) {
+        resultString = i + " * " + j + " = " + i * j;
+
+        expect(spy.calledWithExactly(resultString));
+      }
+    }
+  });
+
+  it("Выводит результат 100 раз", function () {
+    expect(spy.callCount);
+  });
+});
