46cd21e0f691ffeb860db31c29b3e9743285d3f2
DrVladislove <52196631+DrVladislove@users.noreply.github.com>
2020-07-29 17:14:01 +0300

    Add title for Math operations

--- test/test.js
+++ test/test.js
@@ -1,14 +1,16 @@
 const operations = require("./operations.js");
 const assert = require("assert");
 
-it("Правильно находит произведение 2 и 3", () => {
-  assert.equal(operations.multiply(2, 3), 6);
-});
+describe("Математические операции", function () {
+  it("Правильно находит произведение 2 и 3", () => {
+    assert.equal(operations.multiply(2, 3), 6);
+  });
 
-it("Правильно производит деление 9 на 3", () => {
-  assert.equal(operations.divide(9, 3), 3);
-});
+  it("Правильно производит деление 9 на 3", () => {
+    assert.equal(operations.divide(9, 3), 3);
+  });
 
-it("Правильно находит сумму 4 и 3", () => {
-  assert.equal(operations.sum(4, 3), 7);
+  it("Правильно находит сумму 4 и 3", () => {
+    assert.equal(operations.sum(4, 3), 7);
+  });
 });
