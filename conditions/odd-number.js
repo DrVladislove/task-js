22b12880207b8e7c03356071c47e68fb8837cfb2
DrVladislove <52196631+DrVladislove@users.noreply.github.com>
2020-07-30 18:38:01 +0300

    (Task odd number) Add test for function and description of task

--- conditions/odd-number.js
+++ conditions/odd-number.js
@@ -1,14 +1,17 @@
-//Odd Number
+//Из двух случайных чисел, одно из которых четное, а другое нечетное, определить и вывести на экран нечетное число.
 
 function randomizer(min, max) {
   return Math.floor(min + Math.random() * (max + 1 - min));
 }
 
 function oddNumber() {
-  var a = randomizer(1, 1000000);
-  if (a % 2) {
-    console.log(a, "Нечетное");
+  var a = randomizer(1, 1000000),
+    b = randomizer(1, 1000000);
+  if (a % 2 || b % 2) {
+    return "Нечетное";
   }
 }
 
-oddNumber();
+module.exports = {
+  oddNumber,
+};
