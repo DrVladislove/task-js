5e065a5256b1b78275e52068338d1c904664877b
DrVladislove <52196631+DrVladislove@users.noreply.github.com>
2020-07-30 18:20:15 +0300

    (Task equation) add test for function and description of task

--- conditions/equation.js
+++ conditions/equation.js
@@ -1,17 +1,26 @@
+/*
+Найти корни квадратного уравнения и вывести их на экран, если они есть. 
+Если корней нет, то вывести сообщение об этом. Конкретное квадратное уравнение определяется коэффициентами a, b, c, которые вводит пользователь.
+*/
+
 function equation(a, b, c) {
-  var discr = b ** 2 - 4 * a * c,
+  var discr = b * b - 4 * a * c,
     x,
     x1,
     x2;
   if (discr > 0) {
     x1 = (-b + Math.sqrt(discr)) / (2 * a);
     x2 = (-b - Math.sqrt(discr)) / (2 * a);
-    console.log("Первый корень = " + x1 + "; Второй корень = " + x2);
+    return "Первый корень = " + x1 + "; Второй корень = " + x2;
   } else if (discr == 0) {
     x = -b / (2 * a);
-    console.log("Корень = " + x);
+    return "Корень = " + x;
   } else if (discr < 0) {
-    console.log("Корней нет");
+    return "Корней нет";
   }
 }
-equation(9, -6, 2);
+console.log(equation(1, 2, -3));
+
+module.exports = {
+  equation,
+};
