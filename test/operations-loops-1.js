475ead6422f1dcaa69985378292dd9e82296d941
DrVladislove <52196631+DrVladislove@users.noreply.github.com>
2020-07-30 11:56:45 +0300

    (Task numberGenerator) add empty line between functions for readability

--- test/operations-loops-1.js
+++ test/operations-loops-1.js
@@ -6,6 +6,7 @@
 function randomizer(min, max) {
   return Math.floor(min + Math.random() * (max + 1 - min));
 }
+
 function numberGenerator() {
   for (var i = 0; i < 5; i++) {
     var a = randomizer(1, 20);
