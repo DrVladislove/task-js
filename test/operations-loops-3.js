dca4b5ea435bd8e15503fb068c84a65ea8121ab9
DrVladislove <52196631+DrVladislove@users.noreply.github.com>
2020-07-30 12:07:20 +0300

    (Task checkAssignGrade) require function from another file instead of redefining function

--- test/operations-loops-3.js
+++ test/operations-loops-3.js
@@ -4,22 +4,10 @@ Cоздайте функцию checkAssignGrade:
 То есть создайте цикл for от 80 до 100 и вызывайте в нем вашу функцию, например assignGrade(81)
 */
 
-function assignGrade(mark) {
-  if (mark >= 1 && mark <= 20) {
-    return "mark is 1";
-  } else if (mark >= 21 && mark <= 40) {
-    return "mark is 2";
-  } else if (mark >= 41 && mark <= 60) {
-    return "mark is 3";
-  } else if (mark >= 61 && mark <= 80) {
-    return "mark is 4";
-  } else if (mark >= 81 && mark <= 100) {
-    return "mark is 5";
-  }
-}
+var assignFunction = require("./operations-conditions-3.js");
 
 function checkAssignGrade() {
-  var a = assignGrade(81);
+  var a = assignFunction.assignGrade(81);
   for (i = 80; i <= 100; i++) {
     return a;
   }
