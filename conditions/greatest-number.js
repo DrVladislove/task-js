4ca7dd8b40244a0533718769c25756edd7df3930
DrVladislove <52196631+DrVladislove@users.noreply.github.com>
2020-07-30 18:30:21 +0300

    (Task greatest number) Add test for function and description for task

--- conditions/greatest-number.js
+++ conditions/greatest-number.js
@@ -1,10 +1,16 @@
+/*
+Вводятся три целых числа. Определить какое из них наибольшее.
+*/
 function greaterNum(x, y, z) {
   if (x > y && x > z) {
-    console.log(x + " Наибольшее число из трех");
+    return x + " Наибольшее число из трех";
   } else if (y > x && y > z) {
-    console.log(y + " Наибольшее число из трех");
+    return y + " Наибольшее число из трех";
   } else if (z > x && z > y) {
-    console.log(z + " Наибольшее число из трех");
+    return z + " Наибольшее число из трех";
   }
 }
-greaterNum(20, 23, 15);
+
+module.exports = {
+  greaterNum,
+};
