7588b9aad6e38e19c988083a5b1b15e0c2417fec
DrVladislove <52196631+DrVladislove@users.noreply.github.com>
2020-07-30 12:09:54 +0300

    (Task assignGrade) Add empty lines between cases

--- test/operations-conditions-3_test.js
+++ test/operations-conditions-3_test.js
@@ -1,6 +1,3 @@
-/*3. Постановщик оценок
-Напишите функцию assignGrade которая: принимает один аргумент, значение успеваемости от 1 до 100. и возвращает число от 1 до 5: 5, 4, 3, 2, или 1 которое соответствует оценке
-*/
 const operations = require("./operations-conditions-3.js");
 const assert = require("assert");
 
@@ -8,15 +5,19 @@ describe("Постановщик оценок", function () {
   it("Правильно определяет оценку (4)", () => {
     assert.equal(operations.assignGrade(62), "mark is 4");
   });
+
   it("Правильно определяет оценку (5)", () => {
     assert.equal(operations.assignGrade(81), "mark is 5");
   });
+
   it("Правильно определяет оценку (3)", () => {
     assert.equal(operations.assignGrade(57), "mark is 3");
   });
+
   it("Правильно определяет оценку (2)", () => {
     assert.equal(operations.assignGrade(35), "mark is 2");
   });
+
   it("Правильно определяет оценку (1)", () => {
     assert.equal(operations.assignGrade(15), "mark is 1");
   });
