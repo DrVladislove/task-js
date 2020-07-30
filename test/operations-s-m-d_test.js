e472a139883bce2a0d2973eb5190c6a7930d042a
DrVladislove <52196631+DrVladislove@users.noreply.github.com>
2020-07-30 12:20:16 +0300

    (Task Sum, Mul, Div) Rename files, add description of task

---
+++ test/operations-s-m-d_test.js
@@ -0,0 +1,16 @@
+const operations = require("./operations.js");
+const assert = require("assert");
+
+describe("Математические операции", function () {
+  it("Правильно находит произведение 2 и 3", () => {
+    assert.equal(operations.multiply(2, 3), 6);
+  });
+
+  it("Правильно производит деление 9 на 3", () => {
+    assert.equal(operations.divide(9, 3), 3);
+  });
+
+  it("Правильно находит сумму 4 и 3", () => {
+    assert.equal(operations.sum(4, 3), 7);
+  });
+});
