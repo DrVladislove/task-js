f0fddc3f15562e690e862ea1a26c628ec7da9b0a
DrVladislove <52196631+DrVladislove@users.noreply.github.com>
2020-07-31 15:32:48 +0300

    (Task sum-multiply) add description of task and test of function

--- loops/sum-multiply.js
+++ loops/sum-multiply.js
@@ -1,48 +1,7 @@
-/*function sum(a) {
-  var sum = 0;
-  while (a > 0) {
-    sum = Math.trunc(sum) + (a % 10);
-    a = a / 10;
-  }
-  console.log("Сумма: " + sum);
-}
-sum(456);
-*/
-
-const { strict } = require("assert");
-
 /*
-function mul(b) {
-  var mul = 1;
-  while (b) {
-    mul *= b % 10;
-    b = Math.floor(b / 10);
-  }
-  console.log("Произведение: " + mul);
-}
-mul(456);
-
-
-
-function sumMul(a) {
-  var sum = 0,
-    mul = 1;
-  while (a > 0) {
-    console.log(a);
-    var digit = Math.trunc(a % 10);
-    sum = sum + digit;
-    mul *= digit;
-    console.log("Сумма: " + sum);
-    console.log("Произведение: " + mul);
-    a = Math.trunc(a / 10);
-  }
-  console.log("Сумма: " + sum);
-  console.log("Произведение: " + mul);
-}
-sumMul(456);
+Найти сумму и произведение цифр, введенного натурального числа. 
+Например, если введено число 325, то сумма его цифр равна 10 (3+2+5), а произведение 30 (3*2*5).
 */
-
-("use strict");
 
 function sumMul(num, act) {
   switch (act) {
@@ -61,7 +20,10 @@ function sumMul(num, act) {
         .forEach(function (el) {
           mul *= el;
         });
-      console.log(mul);
+      return mul;
   }
 }
-console.log(sumMul(456, "mul"));
+
+module.exports = {
+  sumMul,
+};
