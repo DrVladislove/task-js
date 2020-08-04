30e8fda150807a71c8233fdf77d773437a1a1740
DrVladislove <52196631+DrVladislove@users.noreply.github.com>
2020-08-04 14:42:36 +0300

    (task fibonacci series) add description of task and test of function

--- loops/loops-level-2/fibonacci-series.js
+++ loops/loops-level-2/fibonacci-series.js
@@ -1,3 +1,8 @@
+/*
+Вывести на экран столько элементов ряда Фибоначчи, сколько указал пользователь. 
+Например, если на ввод поступило число 6, то вывод должен содержать шесть первых чисел ряда Фибоначчи: 1 2 3 5 8 13.
+*/
+
 function fibonacciSeries(n) {
   console.log(n, "\n");
   var num1 = 1,
@@ -11,4 +16,7 @@ function fibonacciSeries(n) {
     num2 = a + num1;
   }
 }
-fibonacciSeries(12);
+
+module.exports = {
+  fibonacciSeries,
+};
